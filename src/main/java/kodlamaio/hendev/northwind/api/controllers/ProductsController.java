635bc4eec44b5244d5b5c8a71e14f52756faf2ae
henimex <49072861+henimex@users.noreply.github.com>
2021-05-11 03:13:49 +0300

    Still waiting

--- src/main/java/kodlamaio/hendev/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/hendev/northwind/api/controllers/ProductsController.java
@@ -1,5 +1,30 @@
 package kodlamaio.hendev.northwind.api.controllers;
 
-public class ProductsController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hendev.northwind.business.abstracts.ProductService;
+import kodlamaio.hendev.northwind.entities.concretes.Product;
+
+@RestController
+@RequestMapping("/api/products")
+
+public class ProductsController {
+	
+	private ProductService productService;
+	
+	@Autowired
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+	@GetMapping("/get-all")
+	public List<Product> getAll(){
+		return this.productService.getAll();
+	}
 }
--- src/main/java/kodlamaio/hendev/northwind/business/concretes/ProductManager.java
+++ src/main/java/kodlamaio/hendev/northwind/business/concretes/ProductManager.java
@@ -2,17 +2,27 @@ package kodlamaio.hendev.northwind.business.concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import kodlamaio.hendev.northwind.business.abstracts.ProductService;
+import kodlamaio.hendev.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.hendev.northwind.entities.concretes.Product;
 
+@Service
 public class ProductManager implements ProductService{
 
-	private productDao;
+	private ProductDao productDao;
 	
+	@Autowired
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
 	@Override
 	public List<Product> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.productDao.findAll();
 	}
 
 }
